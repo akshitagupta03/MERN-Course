01cc240267b1e346708e54cfe94ec85a259e0417
Akshita Gupta <akshita.g1507@gmail.com>
2023-07-18 01:48:23 +0530

    file to create appbar in website

---
+++ week-5/day_01/Appbar.jsx
@@ -0,0 +1,44 @@
+import { Button, Typography } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+
+function Appbar(){
+    const navigate = useNavigate()
+
+    return <div style = {{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: 4
+    }}>
+        <div>
+            <Typography variant={"h6"}>Coursera</Typography>
+        </div>
+
+        <div style = {{display: "flex"}}>
+
+            <div style = {{marginRight: 10}}>
+                <Button 
+                    variant={"contained"}
+                    onClick = { () => {
+                        navigate("/signup")
+                    }}
+                >
+                    Sign up
+                </Button>
+            </div>
+
+            <div>
+                <Button 
+                    variant={"contained"}
+                    onClick = { () => {
+                        navigate("/signin")
+                    }}
+                >
+                    Sign in
+                </Button>
+            </div>
+
+        </div> 
+    </div>
+}
+
+export default Appbar;
